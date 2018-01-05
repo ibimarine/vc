fc72b9aa67d2eb0c10b4aa94280378003d7d4ca2
raul doblas <rauldoblasespinosa@gmail.com>
2018-01-05 13:37:56 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from './material.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DatapickerComponent } from './datapicker/datapicker.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    DatapickerComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash: true}),
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
